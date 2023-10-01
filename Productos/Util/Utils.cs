dee6510a2534dab571b4d12014ada602862c9b5b
DDCT2003 <130260043+DDCT2003@users.noreply.github.com>
2023-10-01 12:12:04 -0500

    Agregar archivos de proyecto.

---
+++ Productos/Util/Utils.cs
@@ -0,0 +1,26 @@
+﻿using Productos.Models;
+
+namespace Productos.NewFolder
+{
+    public class Utils
+    {
+
+        public static List<Producto> ListaProducto = new List<Producto>()
+        {
+            new Producto()
+            {
+                IdProducto = 1,
+                Nombre = "Producto 1",
+                Descripcion = "Des",
+                Cantidad = 1 
+            },
+              new Producto()
+            {
+                IdProducto = 2,
+                Nombre = "Producto 2",
+                Descripcion = "Des",
+                Cantidad = 1
+            }
+        };
+    }
+}
